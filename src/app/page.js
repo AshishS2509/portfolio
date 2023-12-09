cd1c4506ad682c0a1e9ec8af15a08aeadaf143f5
Ashish2509 <ashish.2509.k@gmail.com>
2023-12-09 23:17:36 +0530

    Optimized for mobile,etc

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col p-12 justify-evenly items-center lg:flex-row gap-10">
-      <div className="absolute bottom-0 my-10 lg:mx-10 lg:my-0 lg:left-0">
+    <main className="min-h-screen flex flex-col p-12 justify-evenly items-center lg:flex-row gap-10 lg:gap-5">
+      <div className="absolute bottom-0 my-10 lg:mx-10 lg:my-0 lg:left-0 lg:bottom-auto">
         <Navbar />
       </div>
       <div className="col">
@@ -14,13 +14,13 @@ export default function Home() {
       height={400}
       alt="Picture of Ashish Khatik"
       loading="lazy"
-      style={{objectFit: "contain", borderRadius:"100%"}}
+      style={{objectFit: "contain", borderRadius:"100%", border:"12px solid white"}}
     />
       </div>
       <div className="col text-center lg:text-left">
-        <p className='mb-4 text-4xl leading-tight font-bold tracking-tighter'>Hey!!</p>
-        <p className='mb-4 text-4xl leading-tight font-bold tracking-tighter'>My name is <span className='text-yellow-400'>Ashish Khatik</span></p>
-        <p className='mb-4 text-4xl leading-tight font-bold tracking-tighter'>I am a <span className=' text-sky-500'>Full stack web developer</span></p>
+        <p className='mb-4 text-4xl leading-tight font-bold tracking-tighter'><span className='text-yellow-400'>Ashish Khatik</span></p>
+        <p className='mb-4 text-4xl leading-tight font-bold tracking-tighter'><span className='text-blue-500'>Web Developer</span> and Code Enthusiast.</p>
+        <p className='mb-4 text-xl tracking-tighter'>Creating awesome websites with a touch of innovation and fun vibes!</p>
       </div>
     </main>
   )
