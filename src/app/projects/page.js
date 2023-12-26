f125ea6fd1330877a4543c8aed3d43cff01d4b16
Ashish2509 <ashish.2509.k@gmail.com>
2023-12-26 12:52:16 +0530

    Added project, card.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,3 +1,4 @@
+import Card from '@/components/Card'
 import Navbar from '@/components/Navbar'
 import Social from '@/components/Social'
 
@@ -6,7 +7,13 @@ export default function projects() {
     <main className="min-h-screen flex flex-col px-12 py-6 lg:justify-evenly items-center lg:flex-row gap-10 lg:gap-5">
 
         <Navbar current="projects"/>
-
+        <Card 
+        link = "https://qr-generator-gold.vercel.app/"
+        image = "/QR.png"
+        title = "QR-Generator"
+        description = "Create customized QR codes easily with our Next.js QR Generator. Perfect for sharing websites, contact info, and more. It's user-friendly, works on all devices. Trustworthy and quick, it's the simple solution for digital needs."
+        tags = {["Next.js"]}
+        />
         <Social />
       
     </main>
---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+"use client"
+import { Montserrat } from 'next/font/google'
+import Image from 'next/image'
+import React from 'react'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+function Card(props) {
+
+    const link = props.link
+    function handelClick(){
+        window.location.href = link
+    }
+
+  return (
+    <div className=' cursor-pointer' onClick = {handelClick}>
+        <div className="max-w-sm rounded-lg overflow-hidden shadow-gray-700 shadow-md">
+        <Image className="w-full" src={props.image} width={1000} height={700} alt="QR-Generator Screenshot"  />
+        <div className="px-6 py-4">
+            <div className={montserrat.className + " font-bold text-xl mb-2"}>{props.title}</div>
+            <p className="text-gray-700 text-base">
+            {props.description}
+            </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+            {props.tags.map((item, index) => <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{item}</span>)}
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
