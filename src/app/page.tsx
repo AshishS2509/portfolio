81ead4c027afa201f3deb141f8cd2c844c536bb7
AshishS2509 <ashish.solanki.2509@gmail.com>
2026-01-20 23:50:04 +0530

    Added new next fonfig and project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+      <div className="flex justify-center items-center">Ashish Solanki</div>
+      <div className=" flex justify-center lg:justify-end-safe md:justify-end-safe ">Ashish Solanki</div>
+    </div>
+  );
+}
