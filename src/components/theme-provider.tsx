acde2298581698a2d986275cf3a75f87d65cea9a
AshishS2509 <ashish.solanki.2509@gmail.com>
2026-01-25 18:08:31 +0530

    Fixed Card ui, folder structure

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/shared/button";
 
 export function ThemeProvider({
   children,
